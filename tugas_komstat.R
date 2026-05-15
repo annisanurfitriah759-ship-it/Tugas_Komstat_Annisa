f88a4cfa53eaee793f6c3b4c6bd5007903846e3f
Annisa Nurfitriah <annisanurfitriah759@gmail.com>
2026-05-15 23:03:36 +0700

    Menambah fitur max di branch

--- tugas_komstat.R
+++ tugas_komstat.R
@@ -2,3 +2,7 @@
 kopi_terjual <- c(45, 50, 30, 65, 40)
 rata_penjualan <- mean(kopi_terjual)
 print(paste("Rata-rata kopi terjual per hari:", rata_penjualan))
+
+# 3. Mencari nilai penjualan tertinggi
+kopi_max <- max(kopi_terjual)
+print(paste("Penjualan tertinggi minggu ini:", kopi_max))
